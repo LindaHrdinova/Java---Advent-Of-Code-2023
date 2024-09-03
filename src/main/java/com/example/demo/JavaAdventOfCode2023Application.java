73e6a8b94284e26da87107bd3dd5cdf76522560d
Linda Hrdinová <hrlin@seznam.cz>
2024-09-03 20:20:28 +0200

    load line.txt

--- src/main/java/com/example/demo/JavaAdventOfCode2023Application.java
+++ src/main/java/com/example/demo/JavaAdventOfCode2023Application.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-@SpringBootApplication
+import static java.nio.file.Files.readAllLines;
+
 public class JavaAdventOfCode2023Application {
+	public static void main(String[] args) throws IOException {
+        List<String> lines = readAllLines(Path.of("src\\main\\resources\\line.txt"));
+        System.out.println(lines);
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaAdventOfCode2023Application.class, args);
-	}
-
+    }
 }
