be9af955a3e82895f5a9ca2e58eb68bf95bb6c99
Linda Hrdinová <hrlin@seznam.cz>
2024-09-03 20:42:47 +0200

    get digit from line

--- src/main/java/com/example/demo/JavaAdventOfCode2023Application.java
+++ src/main/java/com/example/demo/JavaAdventOfCode2023Application.java
@@ -9,9 +9,21 @@ import java.util.Scanner;
 import static java.nio.file.Files.readAllLines;
 
 public class JavaAdventOfCode2023Application {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         List<String> lines = readAllLines(Path.of("src\\main\\resources\\line.txt"));
         System.out.println(lines);
+
+        for (int i = 0; i < lines.size(); i++) {
+            List<Character> digits = new ArrayList<>();
+            String line = lines.get(i);
+            System.out.println(line);
+            for (int j = 0; j < line.length(); j++) {
+                if (Character.isDigit(line.charAt(j))) {
+                    digits.add(line.charAt(j));
+                }
+            }
+            System.out.println(digits);
+        }
 
     }
 }
