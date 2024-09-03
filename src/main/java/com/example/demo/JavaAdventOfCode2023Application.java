72da4135f6f2845ed55f1367e241d6d990127fd9
Linda Hrdinová <hrlin@seznam.cz>
2024-09-03 23:02:17 +0200

    Trebucher 1.1

--- src/main/java/com/example/demo/JavaAdventOfCode2023Application.java
+++ src/main/java/com/example/demo/JavaAdventOfCode2023Application.java
@@ -10,34 +10,36 @@ import static java.nio.file.Files.readAllLines;
 
 public class JavaAdventOfCode2023Application {
     public static void main(String[] args) throws IOException {
-        List<String> lines = readAllLines(Path.of("src\\main\\resources\\line.txt"));
-        System.out.println(lines);
+        List<String> lines = readAllLines(Path.of("src\\main\\resources\\inputTrebuchet.txt"));
+        System.out.println("Input:");
 
         List<Integer> numbers = new ArrayList<>();
 
+        // for each string in input
         for (int i = 0; i < lines.size(); i++) {
             List<Character> digits = new ArrayList<>();
             String line = lines.get(i);
             System.out.println(line);
+
+            // find digits in string
             for (int j = 0; j < line.length(); j++) {
                 if (Character.isDigit(line.charAt(j))) {
                     digits.add(line.charAt(j));
                 }
             }
-            System.out.println(digits);
+
+            // find first and last digit
             int firstDigit = digits.getFirst();
             int lastDigit = digits.getLast();
             int number = (Character.getNumericValue(firstDigit) * 10) + Character.getNumericValue(lastDigit);
-            System.out.println(number);
-
             numbers.add(number);
         }
-        System.out.println(numbers);
-        int resultNumber = 0;
-        for (int i = 0; i < numbers.size(); i++) {
-            resultNumber += numbers.get(i);
-        }
-        System.out.println(resultNumber);
 
+        // sum of numbers
+        int resultNumber = 0;
+        for (Integer number : numbers) {
+            resultNumber += number;
+        }
+        System.out.println("Result: " + resultNumber);
     }
 }
